3d5c51e6bf42cebbda2f5f1b0030fcfe5acb79a0
Ikhsanfadlylubis <55613953+Ikhsanfadlylubis@users.noreply.github.com>
2019-11-01 13:52:01 +0700

    Add files via upload

---
+++ Program_1914370128_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a,b,c;
+    printf ("masukkan nilai a = 2 .");
+    if (a>0){
+    printf ("masukkan nilai b = 4.");
+    }if ("d>=0"){
+    printf ("masukkan nilai c = 8.");
+    }else{
+    scanf ("%f, %a"),("%f, %b"),("%f, %c");
+    printf ("x1 = -b + sqrt d / 2*a");
+    printf ("x2 = -b + sqrt d / 2*a");
+    printf ("nilai x1 = %, if\n,x1");
+    printf ("nilai x2 = %, if\n,x2");
+    printf ("akar imajiner");
+    }
+  return 0;
+}
+
+
